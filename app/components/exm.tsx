606e4b8d551ffde87aa38af7075ebe2870899c51
Sunasara ShahidAli <127171277+Shahidali00@users.noreply.github.com>
2024-11-18 12:00:58 +0530

    3 commit

--- app/components/exm.tsx
+++ app/components/exm.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useState } from "react";
 
+// Replace var with let or const
 let fstvalue = "";
 let secvalue = "";
 let thdvalue = 0;
 let operator = "";
 
 export default function Calculator() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState<string>("");
 
   const handleClick = (value: string) => {
     setInput(input + value);
@@ -15,50 +16,78 @@ export default function Calculator() {
 
   const handleAns = () => {
     secvalue = input;
-    const [_, secondOperand] = secvalue.split(operator);
-    const firstValue = Number(fstvalue);
-    const secondValue = Number(secondOperand);
-
     switch (operator) {
       case "+":
-        thdvalue = firstValue + secondValue;
+        const [, plusSecondOperand] = secvalue.split("+");
+        thdvalue = Number(fstvalue) + Number(plusSecondOperand);
+        setInput(thdvalue.toString());
         break;
-      case "-":
-        thdvalue = firstValue - secondValue;
-        break;
-      case "*":
-        thdvalue = firstValue * secondValue;
-        break;
-      case "/":
-        if (secondValue === 0) {
-          setInput("Cannot divide by 0");
-          return;
-        }
-        thdvalue = firstValue / secondValue;
-        break;
-      case "%":
-        thdvalue = (firstValue / 100) * secondValue;
-        break;
-      default:
-        setInput("Invalid input");
-        return;
-    }
 
-    setInput(thdvalue.toString());
+      case "-":
+        const [, minusSecondOperand] = secvalue.split("-");
+        thdvalue = Number(fstvalue) - Number(minusSecondOperand);
+        setInput(thdvalue.toString());
+        break;
+
+      case "*":
+        const [, multiplySecondOperand] = secvalue.split("*");
+        thdvalue = Number(fstvalue) * Number(multiplySecondOperand);
+        setInput(thdvalue.toString());
+        break;
+
+      case "/":
+        const [, divideSecondOperand] = secvalue.split("/");
+        if (Number(divideSecondOperand) === 0) {
+          setInput("Cannot divide by 0");
+          break;
+        }
+        thdvalue = Number(fstvalue) / Number(divideSecondOperand);
+        setInput(thdvalue.toString());
+        break;
+
+      case "%":
+        const [, percentSecondOperand] = secvalue.split("%");
+        thdvalue = (Number(fstvalue) / 100) * Number(percentSecondOperand);
+        setInput(thdvalue.toString());
+        break;
+
+      default:
+        setInput("invalid input");
+        break;
+    }
   };
 
   const handleExp = (op: string) => {
     fstvalue = input;
     operator = op;
-    setInput(fstvalue + op);
+    switch (op) {
+      case "+":
+        setInput(fstvalue + "+");
+        break;
+
+      case "-":
+        setInput(fstvalue + "-");
+        break;
+
+      case "*":
+        setInput(fstvalue + "*");
+        break;
+
+      case "/":
+        setInput(fstvalue + "/");
+        break;
+
+      case "%":
+        setInput(fstvalue + "%");
+        break;
+
+      default:
+        break;
+    }
   };
 
   const handleClear = () => {
     setInput("");
-    fstvalue = "";
-    secvalue = "";
-    thdvalue = 0;
-    operator = "";
   };
 
   const handleBS = () => {
@@ -71,40 +100,122 @@ export default function Calculator() {
         <input
           type="text"
           value={input}
-          readOnly
           className="block w-full p-4 text-right text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </div>
       <div className="grid grid-cols-4 gap-2">
-        {/* Buttons */}
-        <button className="bg-white m-1 rounded active:bg-yellow-400" onClick={handleClear}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={handleClear}
+        >
           AC
         </button>
-        <button className="bg-white m-1 active:bg-yellow-400" onClick={handleBS}>
+        <button
+          className="bg-white m-1 active:bg-yellow-400"
+          onClick={handleBS}
+        >
           ←
         </button>
-        <button className="bg-white m-1 rounded active:bg-yellow-400" onClick={() => handleExp("%")}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleExp("%")}
+        >
           %
         </button>
-        <button className="bg-white m-1 rounded active:bg-yellow-400" onClick={() => handleExp("/")}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleExp("/")}
+        >
           /
         </button>
-        {["7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+"].map((btn) => (
-          <button
-            key={btn}
-            className="bg-white m-1 rounded active:bg-yellow-400"
-            onClick={() => (["*", "-", "+", "/"].includes(btn) ? handleExp(btn) : handleClick(btn))}
-          >
-            {btn}
-          </button>
-        ))}
-        <button className="bg-white m-1 rounded col-span-2 active:bg-yellow-400" onClick={() => handleClick("0")}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("7")}
+        >
+          7
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("8")}
+        >
+          8
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("9")}
+        >
+          9
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleExp("*")}
+        >
+          X
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("4")}
+        >
+          4
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("5")}
+        >
+          5
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("6")}
+        >
+          6
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleExp("-")}
+        >
+          -
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("1")}
+        >
+          1
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("2")}
+        >
+          2
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick("3")}
+        >
+          3
+        </button>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleExp("+")}
+        >
+          +
+        </button>
+        <button
+          className="bg-white m-1 rounded col-span-2 active:bg-yellow-400"
+          onClick={() => handleClick("0")}
+        >
           0
         </button>
-        <button className="bg-white m-1 rounded active:bg-yellow-400" onClick={() => handleClick(".")}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={() => handleClick(".")}
+        >
           .
         </button>
-        <button className="bg-white m-1 rounded active:bg-yellow-400" onClick={handleAns}>
+        <button
+          className="bg-white m-1 rounded active:bg-yellow-400"
+          onClick={handleAns}
+        >
           =
         </button>
       </div>
